dfedbdb43b242bdac088928a54085ad4a1d3c996
Minh Doan <mdoan2001@gmail.com>
2022-06-24 16:55:37 +0700

    [#13]Design slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+import 'slick-carousel/slick/slick';
 
 window.Vue = require('vue').default;
 
@@ -26,7 +27,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+import 'slick-carousel/slick/slick';
+require('./components/slider');
+
 
 const app = new Vue({
     el: '#app',
 });
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -150,4 +150,92 @@
     </div>
 </section>
 
+<section class="feedback">
+    <div class="feedback-top">
+        <div class="title" id="title">Feedback</div>
+        <div class="des">What other students turned professionals have to say about us after learning with us and reaching their goals</div>
+    </div>
+    <div class="container">
+        <div class="slider">
+            <div class="slider-item">
+                <div class="slider-message">
+                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
+                </div>
+                <div class="slider-user">
+                    <img src="/images/user-avatar.png" alt="" class="user-avatar">
+                    <div class="user-info">
+                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-language">PHP</div>
+                        <div class="user-stars">
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item">
+                <div class="slider-message">
+                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
+                </div>
+                <div class="slider-user">
+                    <img src="/images/user-avatar.png" alt="" class="user-avatar">
+                    <div class="user-info">
+                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-language">PHP</div>
+                        <div class="user-stars">
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star-half-stroke user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item">
+                <div class="slider-message">
+                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
+                </div>
+                <div class="slider-user">
+                    <img src="/images/user-avatar.png" alt="" class="user-avatar">
+                    <div class="user-info">
+                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-language">PHP</div>
+                        <div class="user-stars">
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star-half-stroke user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item">
+                <div class="slider-message">
+                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
+                </div>
+                <div class="slider-user">
+                    <img src="/images/user-avatar.png" alt="" class="user-avatar">
+                    <div class="user-info">
+                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-language">PHP</div>
+                        <div class="user-stars">
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star-half-stroke user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
