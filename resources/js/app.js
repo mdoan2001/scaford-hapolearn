fc0093e8e26d209e288dd09a057a2b972fb98625
DOAN <mdoan2001@gmail.com>
2022-07-27 10:48:11 +0700

    [#30] Design course detail page

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ window.Vue = require('vue').default;
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 import 'slick-carousel/slick/slick';
 require('./components/slider');
+require('./components/course_detail');
 
 const app = new Vue({
     el: '#app',
